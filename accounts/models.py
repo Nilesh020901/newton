5f5bcf3745fa458a8200a353636a917e66c2d010
Nilesh020901 <nileshsoni0209@gmail.com>
2024-09-04 11:31:12 +0530

    try

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from .manager import UserManager
 # Create your models here.
+
+class GFG(AbstractUser):
+    phone_number = models.CharField(max_length=12, unique=True)
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELD = []
+    objects = UserManager()
