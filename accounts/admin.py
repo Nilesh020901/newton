ea6ccf2403a586696b730b6ba8c86b45617adb99
Nilesh020901 <nileshsoni0209@gmail.com>
2024-09-04 13:56:56 +0530

    update lunch need to test

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import GFG
+admin.site.register(GFG)
 # Register your models here.
--- accounts/manager.py
+++ accounts/manager.py
@@ -6,3 +6,11 @@ class UserManager(BaseUserManager):
         if not phone:
             raise ValueError('Phone number is required')
             user = self.model(phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    def superuser(self, phone, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        return self.create(phone, password, **extra_fields)
