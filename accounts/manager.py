571fd52144d98a7bb1a840259a75c4f28d3ad81d
Nilesh020901 <nileshsoni0209@gmail.com>
2024-09-04 11:34:38 +0530

    try

--- accounts/manager.py
+++ accounts/manager.py
@@ -2,4 +2,7 @@ from django.contrib.auth.base_user import BaseUserManager
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def create(self, phone, password)
+    def create(self, phone, password=None, **extra_fields):
+        if not phone:
+            raise ValueError('Phone number is required')
+            user = self.model(phone=phone, **extra_fields)
